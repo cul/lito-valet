05c7d34abbc56734039b0c644cb2e85fe3b50919
Stuart Marquis <stuartmarquis@gmail.com>
2022-06-06 14:52:24 -0400

    UNIX-8313 - update Valet deploy scripts for Alma VMs

--- config/deploy/valet_dev.rb
+++ config/deploy/valet_dev.rb
@@ -1,4 +1,22 @@
 
-server 'valet-dev.cul.columbia.edu', user: 'litoserv', roles: %w(app db web)
+# server 'valet-dev.cul.columbia.edu', user: 'litoserv', roles: %w(app db web)
+# set :deploy_to, '/opt/passenger/lito/valet_dev'
 
-set :deploy_to, '/opt/passenger/lito/valet_dev'
+# Alma
+server 'lito-rails-dev1.cul.columbia.edu', user: 'litoserv', roles: %w(app db web)
+set :deploy_to, '/opt/passenger/valet_dev'
+set :rvm_ruby_version, 'valet_alma'
+
+# If for some reason we're not on campus or on VPN,
+# we can deploy via a jumphost, as follows:
+# (taken from https://gist.github.com/peterhellberg/e823e3f73495e17f1f02 )
+# set :user, 'litoserv'
+# set :jumphost, 'connect.cul.columbia.edu'
+#
+# set :ssh_options, {
+#   user: fetch(:user),
+#   forward_agent: false,
+#   proxy: Net::SSH::Proxy::Command.new(
+#     "ssh -l #{fetch(:user)} #{fetch(:jumphost)} -W %h:%p"
+#   )
+# }
--- config/deploy/valet_test.rb
+++ config/deploy/valet_test.rb
@@ -1,4 +1,11 @@
 
-server 'valet-test.cul.columbia.edu', user: 'litoserv', roles: %w(app db web)
+# server 'valet-test.cul.columbia.edu', user: 'litoserv', roles: %w(app db web)
+#
+# set :deploy_to, '/opt/passenger/lito/valet_test'
 
-set :deploy_to, '/opt/passenger/lito/valet_test'
+
+# Alma
+server 'lito-rails-test1.cul.columbia.edu', user: 'litoserv', roles: %w(app db web)
+set :deploy_to, '/opt/passenger/valet_test'
+set :rvm_ruby_version, 'valet_alma'
+
