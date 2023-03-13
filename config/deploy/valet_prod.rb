c14665127b6455c41bc5c7b7ae707fcbb810535e
Stuart Marquis <stuartmarquis@gmail.com>
2023-03-13 10:58:39 -0400

    RVM alias name, force_ssl

--- config/deploy/valet_prod.rb
+++ config/deploy/valet_prod.rb
@@ -7,5 +7,5 @@
 # Alma
 server 'lito-rails-prod1.cul.columbia.edu', user: 'litoserv', roles: %w(app db web)
 set :deploy_to, '/opt/passenger/valet_prod'
-set :rvm_ruby_version, 'valet_alma'
+set :rvm_ruby_version, 'valet'
 
--- config/deploy/valet_test.rb
+++ config/deploy/valet_test.rb
@@ -7,5 +7,5 @@
 # Alma
 server 'lito-rails-test1.cul.columbia.edu', user: 'litoserv', roles: %w(app db web)
 set :deploy_to, '/opt/passenger/valet_test'
-set :rvm_ruby_version, 'valet_alma'
+set :rvm_ruby_version, 'valet'
 
