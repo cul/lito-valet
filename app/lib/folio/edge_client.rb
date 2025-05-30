92769b0b942dddbf0a3c797af6181fe8d5279342
Stuart Marquis <stuartmarquis@gmail.com>
2025-05-30 17:44:06 -0400

    disable ../folio/edge_client.rb - never needed, never built

--- app/lib/folio/edge_client.rb
+++ app/lib/folio/edge_client.rb
@@ -1,11 +1,14 @@
-# 
-# FOLIO DOCUMENTATION
-#
-# Edge RTAC:
-#   https://folio-org.atlassian.net/wiki/spaces/FOLIJET/pages/1384120/RTAC
-#   https://s3.amazonaws.com/foliodocs/api/edge-rtac/p/edge-rtac.html
-#   https://github.com/folio-org/edge-rtac/blob/master/ramls/edge-rtac.raml
-# 
 
-module Folio
-  class OkapiClient
+#  I don't think we need this at all anymore?
+
+# #
+# # FOLIO DOCUMENTATION
+# #
+# # Edge RTAC:
+# #   https://folio-org.atlassian.net/wiki/spaces/FOLIJET/pages/1384120/RTAC
+# #   https://s3.amazonaws.com/foliodocs/api/edge-rtac/p/edge-rtac.html
+# #   https://github.com/folio-org/edge-rtac/blob/master/ramls/edge-rtac.raml
+# #
+#
+# module Folio
+#   class OkapiClient
